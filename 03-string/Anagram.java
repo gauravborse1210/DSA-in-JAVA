cb86b6d9e1df3babf746c32aef2133f94cc7bf88
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-01-25 17:42:56 +0530

    Add files via upload

---
+++ 03-string/Anagram.java
@@ -0,0 +1,25 @@
+public class Anagram {
+
+  public static boolean AnagramString(String str1, String str2){
+    boolean isAnagram = false;
+
+    for(int i = 0; i < str1.length(); i++){
+      if(str1.length() != str2.length()){
+        break;
+      }
+      for(int j = 0; j < str2.length(); j++){
+        if(str1.charAt(i) == str2.charAt(j)){
+          isAnagram = true;
+          break;
+        }
+      }
+    }
+    return isAnagram;
+  }
+
+  public static void main(String args[]){
+    String str1 = "race";
+    String str2 = "care";
+    System.out.print(AnagramString(str1, str2));
+  }
+}
