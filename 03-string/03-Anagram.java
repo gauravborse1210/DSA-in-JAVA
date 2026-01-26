25d68e3208165a4d4ae7fb8d08722f0b01ed5090
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-01-26 16:10:37 +0530

    Update 03-Anagram.java

--- 03-string/03-Anagram.java
+++ 03-string/03-Anagram.java
@@ -3,10 +3,11 @@ public class Anagram {
   public static boolean AnagramString(String str1, String str2){
     boolean isAnagram = false;
 
-    for(int i = 0; i < str1.length(); i++){
-      if(str1.length() != str2.length()){
-        break;
+     if(str1.length() != str2.length()){
+        isAnagram = false;
       }
+
+    for(int i = 0; i < str1.length(); i++){
       for(int j = 0; j < str2.length(); j++){
         if(str1.charAt(i) == str2.charAt(j)){
           isAnagram = true;
@@ -23,3 +24,4 @@ public class Anagram {
     System.out.print(AnagramString(str1, str2));
   }
 }
+
