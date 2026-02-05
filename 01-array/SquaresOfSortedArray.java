d29dc6bcd87ff76bdb70ddb0524572c3d3383447
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-02-05 16:44:14 +0530

    Add files via upload

---
+++ 01-array/SquaresOfSortedArray.java
@@ -0,0 +1,23 @@
+class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int sortedArr[] = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int idx = nums.length - 1;
+
+        while(idx >= 0){
+            if(nums[i] * nums[i] <= nums[j] * nums[j]){
+                sortedArr[idx] = nums[j] * nums[j];
+                idx--;
+                j--;
+            }else{
+                sortedArr[idx] = nums[i] * nums[i];
+                idx--;
+                i++;
+            }
+        }
+
+        return sortedArr;
+        
+    }
+}
