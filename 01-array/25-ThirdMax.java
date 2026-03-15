d75fd6cbb1ac1a4f77f5c9bf123aa72e6589f93b
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-03-15 19:38:31 +0530

    Refactor Solution class for thirdMax method

--- 01-array/25-ThirdMax.java
+++ 01-array/25-ThirdMax.java
@@ -3,16 +3,19 @@ class Solution {
         long firstMax = Long.MIN_VALUE;
         long secMax = Long.MIN_VALUE;
         long thirdMax = Long.MIN_VALUE;
+        
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > firstMax){
                 firstMax = nums[i];
             }
         }
+        
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > secMax && nums[i] != firstMax){
                 secMax = nums[i];
             }
         }
+        
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > thirdMax && nums[i] != firstMax && nums[i] != secMax){
                 thirdMax = nums[i];
@@ -22,6 +25,7 @@ class Solution {
             return (int) firstMax;
         }
 
+        
         return (int) thirdMax;
     }
 }
