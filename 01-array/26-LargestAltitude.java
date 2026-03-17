4cce04853c467ca562c007cda881b4ec60c5e964
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-03-17 16:51:06 +0530

    Update 26-LargestAltitude.java

--- 01-array/26-LargestAltitude.java
+++ 01-array/26-LargestAltitude.java
@@ -1,13 +1,20 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int maxAltitude = 0;
-        int currentAltitude = 0;
-        
-        for (int i = 0; i < gain.length; i++) {
-            currentAltitude += gain[i];
-            maxAltitude = Math.max(maxAltitude, currentAltitude);
+
+        int max = 0;
+        int[] result = new int[gain.length + 1];
+        result[0] = 0;
+
+        for(int i = 1; i <= gain.length; i++){
+            result[i] = result[i-1] + gain[i-1];
         }
 
-        return maxAltitude;
+        for(int i = 0; i < result.length; i++){
+            if(max < result[i]){
+                max = result[i];
+            }
+        }
+
+        return max;
     }
 }
