a83a9380e2072dea8613c9e402451feaf0deb96b
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-03-19 16:22:16 +0530

    Update 04-TrappingRainWater.java

--- 01-array/04-TrappingRainWater.java
+++ 01-array/04-TrappingRainWater.java
@@ -33,13 +33,5 @@ public class TrappingRainWater {
 
         return water;
     }
-
-    public static void main(String args[]){
-        TrappingRainWater sol = new TrappingRainWater(); 
-        int array[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int result = sol.trap(array);   
-        System.out.println(result);
-    }
-
 }
 
