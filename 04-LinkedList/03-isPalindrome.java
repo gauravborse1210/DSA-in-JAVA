6c0cac1b046b5e6f9036e2ab684b0e338c75f56f
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-03-31 15:13:42 +0530

    Add files via upload

---
+++ 04-LinkedList/03-isPalindrome.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Find Middle node
+        ListNode midNode = slow;
+
+        // Reverse 2nd half
+        ListNode prev = null;
+        ListNode next;
+        while(midNode != null){
+            next = midNode.next;
+            midNode.next = prev;
+            prev = midNode;
+            midNode = next;
+        }
+
+        // Check left and right half
+        ListNode left = head;
+        ListNode right = prev;
+
+        while(right != null){
+            if(left.val != right.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
