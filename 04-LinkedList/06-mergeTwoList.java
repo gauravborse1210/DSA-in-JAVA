c79b0e3516845f6c6aba58467e71368b7251df5e
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-04-02 23:55:12 +0530

    Update 06-mergeTwoList.java

--- 04-LinkedList/06-mergeTwoList.java
+++ 04-LinkedList/06-mergeTwoList.java
@@ -8,25 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode();
-        ListNode cur = dummy;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {   
 
-        while (list1 != null && list2 != null) {
-            if (list1.val > list2.val) {
-                cur.next = list2;
+        ListNode head = new ListNode();
+        ListNode temp = head;
+
+        while(list1 != null && list2 != null){
+            if(list1.val > list2.val){
+                temp.next = list2;
                 list2 = list2.next;
-            } else {
-                cur.next = list1;
+            }else{
+                temp.next = list1;
                 list1 = list1.next;
             }
-            cur = cur.next;
-        }
 
-        cur.next = (list1 != null) ? list1 : list2;
+            temp = temp.next;
+        }   
 
-        return dummy.next;        
+        temp.next = list1 != null ? list1 : list2;
+
+        return head.next;
     }
 }
+
+
+
+
+
+
+
+
+
+
