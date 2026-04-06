372952c68be79bac783a21111c4e7a223eb88c4b
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-04-06 18:47:23 +0530

    Create 01-fibonacci.java

---
+++ 05-Recurssion/01-fibonacci.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int fib(int n) {
+        // USING RECURSION
+        // if(n == 0){
+        //     return 0;
+        // }
+
+        // if(n == 1){
+        //     return 1;
+        // }
+
+        // int result = fib(n - 2) + fib(n - 1);
+
+        // return result;
+
+        //USING LINEAR WAY
+         if(n == 0){
+            return 0;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr; 
+        }
+
+        return prev1;
+    }
+}
