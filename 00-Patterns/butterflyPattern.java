94ff5b2a3e28c3e5c835e5f59113acd993702bab
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-04-26 18:27:09 +0530

    Add files via upload

---
+++ 00-Patterns/butterflyPattern.java
@@ -0,0 +1,44 @@
+public class butterflyPattern {
+  public static void butterfly_pattern(int n){
+    for(int i = 1; i <= n; i++){
+      // stars
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+
+      // spaces
+      for(int j = 1; j <= 2 * (n - i); j++){
+        System.out.print(" ");
+      }
+
+      // stars
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }  
+      System.out.println();   
+    }
+
+    for(int i = n; i >= 1; i--){
+      // stars
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+
+      // spaces
+      for(int j = 1; j <= 2 * (n - i); j++){
+        System.out.print(" ");
+      }
+
+      // stars
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }  
+      System.out.println();
+    }
+  }
+  public static void main(String args[]){
+    int n = 4;
+
+    butterfly_pattern(n);
+  }
+}
