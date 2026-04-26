88ee2985ed624789bc15e434c91451606e27962a
Gaurav Narayan Borse <143621246+gauravborse1210@users.noreply.github.com>
2026-04-26 18:28:12 +0530

    Add files via upload

---
+++ 00-Patterns/invertedAndRotatedHalfPyramid.java
@@ -0,0 +1,22 @@
+public class invertedAndRotatedHalfPyramid {
+
+  public static void inverted_and_rotated_half_pyramid(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= n-i; j++){
+        System.out.print(" ");
+      }
+
+      for(int k = 1; k <= i; k++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String args[]){
+
+    int n = 4;
+
+    inverted_and_rotated_half_pyramid(n);
+  }
+}
